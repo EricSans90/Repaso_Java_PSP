1511affdd1dc18f0eb88a43a24433ed0b574784e
Eric_Sans <esansvi@gmail.com>
2023-09-10 21:45:49 +0200

    a

---
+++ src/Ejercicio1.java
@@ -0,0 +1,6 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Persona P1 = new Persona("Juan");
+        System.out.println(P1.getNombre());
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Persona (String nombre){
+        this.nombre = nombre;
+    }
+}
